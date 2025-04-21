4a99fc72a49fe4277523daa409d87b2a411708df
aljonleynes11 <aljonleynes11@gmail.com>
2025-04-21 11:40:29 +0800

    fix: display-name-warning

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { registerRootComponent } from 'expo';
 import { ExpoRoot } from 'expo-router';
 
-export const App = require('expo-router/entry').default;
+const App = require('expo-router/entry').default;
 
 registerRootComponent(App);
+
+export default App;
